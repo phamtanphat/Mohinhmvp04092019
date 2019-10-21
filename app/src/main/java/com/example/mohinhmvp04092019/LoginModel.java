fffcd1ce2e261b35b170c26fd076a12a75b3a0bf
phatdroid94 <phatdroid94@gmail.com>
2019-10-21 20:34:06 +0700

    regex email

--- app/src/main/java/com/example/mohinhmvp04092019/LoginModel.java
+++ app/src/main/java/com/example/mohinhmvp04092019/LoginModel.java
@@ -1,10 +1,17 @@
 package com.example.mohinhmvp04092019;
 
+import java.util.regex.Pattern;
+
 public class LoginModel {
 
+    String regexEmail = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
 
     public void checkLogin(String user, String password) {
 
+        if (user == null){
+
+
+        }else if()
 
     }
 }
