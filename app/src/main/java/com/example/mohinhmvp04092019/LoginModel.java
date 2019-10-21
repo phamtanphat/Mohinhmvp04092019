f1b36986fb1ef0dbe97c22d6f9708539d19eb483
phatdroid94 <phatdroid94@gmail.com>
2019-10-21 20:49:00 +0700

    xử lý xong tầng bussinessmodel

--- app/src/main/java/com/example/mohinhmvp04092019/LoginModel.java
+++ app/src/main/java/com/example/mohinhmvp04092019/LoginModel.java
@@ -1,17 +1,31 @@
 package com.example.mohinhmvp04092019;
 
+import java.util.Objects;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoginModel {
 
     String regexEmail = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
 
+    private ResponseModel responseModel;
+
+    public LoginModel(ResponseModel responseModel) {
+       this.responseModel = responseModel;
+    }
     public void checkLogin(String user, String password) {
+        boolean isEmail = Pattern.matches(regexEmail, user);
 
-        if (user == null){
-
-
-        }else if()
-
+        if (user.length() <= 0 && password.length() <= 0){
+            responseModel.parameterIsNull();
+        }else if(!isEmail){
+            responseModel.inCorrectEmail();
+        }else{
+            if (user.equals("phatdroid123@gmail.com") && password.equals("123")){
+                responseModel.loginSuccess();
+            }else{
+                responseModel.loginFail();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/mohinhmvp04092019/ResponseModel.java
@@ -0,0 +1,8 @@
+package com.example.mohinhmvp04092019;
+
+public interface ResponseModel {
+    void parameterIsNull();
+    void inCorrectEmail();
+    void loginSuccess();
+    void loginFail();
+}
