47cdd0ee71b83dd58cacba295c6aec4852e233d5
phatdroid94 <phatdroid94@gmail.com>
2019-10-21 20:28:25 +0700

    cách gửi dữ liệu cho các tầng trong mô hình

---
+++ app/src/main/java/com/example/mohinhmvp04092019/LoginModel.java
@@ -0,0 +1,10 @@
+package com.example.mohinhmvp04092019;
+
+public class LoginModel {
+
+
+    public void checkLogin(String user, String password) {
+
+
+    }
+}
--- app/src/main/java/com/example/mohinhmvp04092019/MainActivity.java
+++ app/src/main/java/com/example/mohinhmvp04092019/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.mohinhmvp04092019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtUser,edtPassword;
+    Button btnLogin;
+    MainPresenter mainPresenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUser = findViewById(R.id.edittextUser);
+        edtPassword = findViewById(R.id.edittextPassword);
+        btnLogin = findViewById(R.id.buttonLogin);
+        mainPresenter = new MainPresenter();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = edtUser.getText().toString().trim();
+                String password = edtPassword.getText().toString().trim();
+
+                mainPresenter.validateLogin(user,password);
+            }
+        });
     }
 }
